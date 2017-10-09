35ffd0bd1967618dbfe8b2f4088a6dbdf15b3d72
igorsnezko <i.s.sergeevich@yandex.ru>
2017-10-09 13:22:59 +0300

    add download files

--- app/Http/Controllers/Api/V1/Product/DownloadController.php
+++ app/Http/Controllers/Api/V1/Product/DownloadController.php
@@ -10,6 +10,7 @@ namespace App\Http\Controllers\Api\V1\Product;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use JWTAuth;
 
 class DownloadController extends Controller
 {
