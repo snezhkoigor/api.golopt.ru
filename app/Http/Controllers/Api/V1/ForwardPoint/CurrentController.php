184d71137409a1a471f552ed9de851f13bfccd51
igorsnezko <i.s.sergeevich@yandex.ru>
2017-10-11 11:49:05 +0300

    change FP controller

--- app/Http/Controllers/Api/V1/ForwardPoint/CurrentController.php
+++ app/Http/Controllers/Api/V1/ForwardPoint/CurrentController.php
@@ -73,7 +73,7 @@ class CurrentController extends Controller
                         ];
                     }
 
-                    return $result;
+                    return response()->json($result);
                 }
             }
 
