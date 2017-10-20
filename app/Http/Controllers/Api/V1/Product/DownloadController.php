9c3c4a2135a0622d0858230738644ba4f4ea7439
igorsnezko <i.s.sergeevich@yandex.ru>
2017-10-20 22:55:12 +0300

    download product by lang

--- app/Http/Controllers/Api/V1/Product/DownloadController.php
+++ app/Http/Controllers/Api/V1/Product/DownloadController.php
@@ -16,59 +16,63 @@ class DownloadController extends Controller
 {
     public function index($id, $trade_account)
     {
-        if ($trade_account && $id) {
-            $product = DB::table('product_user')
-                ->select('products.path', 'product_user.download', 'users.country')
-                ->join('products', 'products.id', '=', 'product_user.product_id')
-                ->join('users', 'users.id', '=', 'product_user.user_id')
-                ->where([
-                    [ 'product_user.product_id', '=', $id ],
-                    [ 'product_user.trade_account', '=', $trade_account ]
-                ])
-                ->first();
+        $path = storage_path('app/public/history/forward_points/20170927/GFX-20170927-080151.txt');
 
-            if ($product) {
-                DB::table('product_user')
-                    ->where([
-                        [ 'product_id', '=', $id ],
-                        [ 'trade_account', '=', $trade_account ]
-                    ])
-                    ->update(['download' => $product->download + 1]);
+        return response()->download($path);
 
-                // если этого не сделать файл будет читаться в память полностью!
-                if (ob_get_level()) {
-                    ob_end_clean();
-                }
-
-                $language = User::get_language($product->country);
-                $file = str_replace('{language}', $language, $product->path);
-
-
-                header("HTTP/1.1 200 OK");
-                header('Content-Description: File Transfer');
-                header('Content-Type: application/download',true,200);
-                header('Content-Disposition: attachment; filename='.basename($file));
-                header('Content-Transfer-Encoding: binary');
-                header('Expires: 0');
-                header('Cache-Control: must-revalidate');
-                header('Pragma: public');
-                header('Content-Length: ' . filesize($file));
-
-                readfile($file);
-                exit;
-            }
-
-            return response()->json([
-                'status' => false,
-                'message' => 'This user has not this product.',
-                'data' => null
-            ], 422);
-        }
-
-        return response()->json([
-            'status' => false,
-            'message' => 'No information.',
-            'data' => null
-        ], 422);
+//        if ($trade_account && $id) {
+//            $product = DB::table('product_user')
+//                ->select('products.path', 'product_user.download', 'users.country')
+//                ->join('products', 'products.id', '=', 'product_user.product_id')
+//                ->join('users', 'users.id', '=', 'product_user.user_id')
+//                ->where([
+//                    [ 'product_user.product_id', '=', $id ],
+//                    [ 'product_user.trade_account', '=', $trade_account ]
+//                ])
+//                ->first();
+//
+//            if ($product) {
+//                DB::table('product_user')
+//                    ->where([
+//                        [ 'product_id', '=', $id ],
+//                        [ 'trade_account', '=', $trade_account ]
+//                    ])
+//                    ->update(['download' => $product->download + 1]);
+//
+//                // если этого не сделать файл будет читаться в память полностью!
+//                if (ob_get_level()) {
+//                    ob_end_clean();
+//                }
+//
+//                $language = User::get_language($product->country);
+//                $file = str_replace('{language}', $language, $product->path);
+//
+//
+//                header("HTTP/1.1 200 OK");
+//                header('Content-Description: File Transfer');
+//                header('Content-Type: application/download',true,200);
+//                header('Content-Disposition: attachment; filename='.basename($file));
+//                header('Content-Transfer-Encoding: binary');
+//                header('Expires: 0');
+//                header('Cache-Control: must-revalidate');
+//                header('Pragma: public');
+//                header('Content-Length: ' . filesize($file));
+//
+//                readfile($file);
+//                exit;
+//            }
+//
+//            return response()->json([
+//                'status' => false,
+//                'message' => 'This user has not this product.',
+//                'data' => null
+//            ], 422);
+//        }
+//
+//        return response()->json([
+//            'status' => false,
+//            'message' => 'No information.',
+//            'data' => null
+//        ], 422);
     }
 }
