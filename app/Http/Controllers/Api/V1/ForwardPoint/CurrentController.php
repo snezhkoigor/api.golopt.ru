8fc3f9772020be8393b4e52093bced1471bef1d3
igorsnezko <i.s.sergeevich@yandex.ru>
2017-11-01 00:03:05 +0300

    add 5542813

--- app/Http/Controllers/Api/V1/ForwardPoint/CurrentController.php
+++ app/Http/Controllers/Api/V1/ForwardPoint/CurrentController.php
@@ -82,4 +82,53 @@ class CurrentController extends Controller
             'data' => null
         ], 422);
     }
+
+	public function newGet(Request $request, $account, $pair)
+	{
+		$validator = Validator::make([ 'account' => $account ], $this->rules(), $this->messages());
+
+		if ($validator->fails() === false) {
+			$accountInfo = DB::table('product_user')
+				->where([
+					[ 'trade_account', '=', $account ],
+					[ 'active', '=', 1 ]
+				])
+				->first();
+
+			if ($accountInfo) {
+				$date = ($request->get('date') ? $request->get('date') : date('Y-m-d'));
+				if (in_array(date('w'), [0, 6]) && !$request->get('date')) {
+					if (date('w') === 6) {
+						$date = date('Y-m-d', strtotime('-1 DAY'));
+					} else {
+						$date = date('Y-m-d', strtotime('-2 DAY'));
+					}
+				}
+
+				$fp = DB::table('forward_points')
+					->select('forward_points.name', 'forward_points.fp', DB::raw('UNIX_TIMESTAMP(forward_points.updated_at) as updated_at'))
+					->where([
+						[ 'forward_points.date', '=', $date ],
+						[ 'forward_points.name', '=', $pair ]
+					])
+					->first();
+
+				if ($fp) {
+					echo (float)$fp->fp;
+				}
+			}
+
+			return response()->json([
+				'status' => 0,
+				'message' => 'No forward points. (ERR - 1)',
+				'data' => null
+			], 422);
+		}
+
+		return response()->json([
+			'status' => 0,
+			'message' => 'No forward points. (ERR - 2)',
+			'data' => null
+		], 422);
+	}
 }
