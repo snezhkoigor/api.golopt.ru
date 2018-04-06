dc2c4b6a078794d9c83bb9004ca14f8acd288fc1
igorsnezko <i.s.sergeevich@yandex.ru>
2018-04-06 15:26:04 +0300

    get futures from cme

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 		$schedule->command('getFuturesFromCME')
-			->hourly()
+			->everyThirtyMinutes()
 			->withoutOverlapping();
 
 
