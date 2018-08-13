502bb28faf7c6ab666658572a3f519b6985b5758
snezhkoigor <i.s.sergeevich@yandex.ru>
2018-08-13 11:45:26 +0300

    Update CurrentController.php

--- app/Http/Controllers/Api/V1/ForwardPoint/CurrentController.php
+++ app/Http/Controllers/Api/V1/ForwardPoint/CurrentController.php
@@ -110,101 +110,101 @@ class CurrentController extends Controller
 //		CAD,-16.1
 //		CHF,-51.0
 //		JPY,-43.6
-		$content = file_get_contents('http://goloption.ru/Files/FP/Forward_Point.csv');
-		if ($content)
-		{
-			$array = explode(';', $content);
-			switch (strtolower($pair))
-			{
-				case 'eur':
-				case 'eurusd':
-					echo (float)$array[3];
-					die;
+// 		$content = file_get_contents('http://goloption.ru/Files/FP/Forward_Point.csv');
+// 		if ($content)
+// 		{
+// 			$array = explode(';', $content);
+// 			switch (strtolower($pair))
+// 			{
+// 				case 'eur':
+// 				case 'eurusd':
+// 					echo (float)$array[3];
+// 					die;
 					
-				case 'gbp':
-				case 'gbpusd':
-					echo (float)$array[4];
-					die;
+// 				case 'gbp':
+// 				case 'gbpusd':
+// 					echo (float)$array[4];
+// 					die;
 					
-				case 'aud':
-				case 'audusd':
-					echo (float)$array[0];
-					die;
+// 				case 'aud':
+// 				case 'audusd':
+// 					echo (float)$array[0];
+// 					die;
 					
-				case 'cad':
-				case 'usdcad':
-					echo (float)$array[1];
-					die;
+// 				case 'cad':
+// 				case 'usdcad':
+// 					echo (float)$array[1];
+// 					die;
 					
-				case 'chf':
-				case 'usdchf':
-					echo (float)$array[2];
-					die;
+// 				case 'chf':
+// 				case 'usdchf':
+// 					echo (float)$array[2];
+// 					die;
 					
-				case 'jpy':
-				case 'usdjpy':
-					echo (float)$array[5];
-					die;
+// 				case 'jpy':
+// 				case 'usdjpy':
+// 					echo (float)$array[5];
+// 					die;
+// 			}
+// 		}
+
+// 		echo 0;
+// 		die;
+		
+		$date = ($request->get('date') ? $request->get('date') : date('Y-m-d'));
+		if (in_array(date('w'), [0, 6]) && !$request->get('date')) {
+			if (date('w') === 6) {
+				$date = date('Y-m-d', strtotime('-1 DAY'));
+			} else {
+				$date = date('Y-m-d', strtotime('-2 DAY'));
 			}
+		}
+
+		if (in_array($account, User::getDevAccounts())) {
+			$fp = DB::table('forward_points')
+				->select('forward_points.name', 'forward_points.fp', DB::raw('UNIX_TIMESTAMP(forward_points.updated_at) as updated_at'))
+				->where([
+					[ 'forward_points.date', '=', $date ],
+					[ 'forward_points.name', '=', $pair ]
+				])
+				->first();
+
+			if ($fp) {
+				echo (float)$fp->fp;
+				die;
+			}
+		}
+
+		$validator = Validator::make([ 'account' => $account ], $this->rules(), $this->messages());
+
+		if ($validator->fails() === false) {
+			$accountInfo = DB::table('product_user')
+				->where([
+					[ 'trade_account', '=', $account ],
+					[ 'active', '=', 1 ]
+				])
+				->first();
+
+			if ($accountInfo) {
+				$fp = DB::table('forward_points')
+					->select('forward_points.name', 'forward_points.fp', DB::raw('UNIX_TIMESTAMP(forward_points.updated_at) as updated_at'))
+					->where([
+						[ 'forward_points.date', '=', $date ],
+						[ 'forward_points.name', '=', $pair ]
+					])
+					->first();
+
+				if ($fp) {
+					echo (float)$fp->fp;
+					die;
+				}
+			}
+
+			echo 0;
+			die;
 		}
 
 		echo 0;
 		die;
-		
-//		$date = ($request->get('date') ? $request->get('date') : date('Y-m-d'));
-//		if (in_array(date('w'), [0, 6]) && !$request->get('date')) {
-//			if (date('w') === 6) {
-//				$date = date('Y-m-d', strtotime('-1 DAY'));
-//			} else {
-//				$date = date('Y-m-d', strtotime('-2 DAY'));
-//			}
-//		}
-//
-//		if (in_array($account, User::getDevAccounts())) {
-//			$fp = DB::table('forward_points')
-//				->select('forward_points.name', 'forward_points.fp', DB::raw('UNIX_TIMESTAMP(forward_points.updated_at) as updated_at'))
-//				->where([
-//					[ 'forward_points.date', '=', $date ],
-//					[ 'forward_points.name', '=', $pair ]
-//				])
-//				->first();
-//
-//			if ($fp) {
-//				echo (float)$fp->fp;
-//				die;
-//			}
-//		}
-//
-//		$validator = Validator::make([ 'account' => $account ], $this->rules(), $this->messages());
-//
-//		if ($validator->fails() === false) {
-//			$accountInfo = DB::table('product_user')
-//				->where([
-//					[ 'trade_account', '=', $account ],
-//					[ 'active', '=', 1 ]
-//				])
-//				->first();
-//
-//			if ($accountInfo) {
-//				$fp = DB::table('forward_points')
-//					->select('forward_points.name', 'forward_points.fp', DB::raw('UNIX_TIMESTAMP(forward_points.updated_at) as updated_at'))
-//					->where([
-//						[ 'forward_points.date', '=', $date ],
-//						[ 'forward_points.name', '=', $pair ]
-//					])
-//					->first();
-//
-//				if ($fp) {
-//					echo (float)$fp->fp;
-//					die;
-//				}
-//			}
-//
-//			echo 0;
-//			die;
-//		}
-//
-//		echo 0;
-//		die;
 	}
 }
