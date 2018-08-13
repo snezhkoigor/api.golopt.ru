24eb3b6fc33a56c722675ca5b4b04b56423eec9a
snezhkoigor <i.s.sergeevich@yandex.ru>
2018-08-13 11:42:24 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,8 @@ Route::group(['middleware' => ['api', 'cors'], 'namespace' => 'Api\V1'], functio
     // For indicators
     Route::get('/user/subscription/verification', 'User\SubscriptionVerificationController@index');
     Route::get('/forward-points/{account}/{pair}/get', 'ForwardPoint\CurrentController@index');
-	Route::get('/forward-points/{account}/{pair}', 'ForwardPoint\CurrentController@newGet');
+	Route::get('/forward-points/{account}/{pair}', 'ForwardPoint\CurrentController@index');
+// 	Route::get('/forward-points/{account}/{pair}', 'ForwardPoint\CurrentController@newGet');
 	Route::get('/futures/{account}/{pair}', 'Futures\CurrentController@get');
 
 	// News
