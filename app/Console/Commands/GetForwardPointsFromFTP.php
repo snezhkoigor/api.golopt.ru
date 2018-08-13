6ef788fd8fa47c867def05546c9c6a036e61e5b7
snezhkoigor <i.s.sergeevich@yandex.ru>
2018-08-13 10:54:41 +0300

    Update GetForwardPointsFromFTP.php

--- app/Console/Commands/GetForwardPointsFromFTP.php
+++ app/Console/Commands/GetForwardPointsFromFTP.php
@@ -43,45 +43,49 @@ class GetForwardPointsFromFTP extends Command
         $disk = Storage::disk('public');
         $path_prefix = $disk->getDriver()->getAdapter()->getPathPrefix();
 
+        $conn_id = ftp_connect('http://bulatlab.ru/forwardpoint/');
+        ftp_pasv($conn_id, true);
+        $contents = ftp_nlist($conn_id, '/');
+        var_dump($contents);die;
         // установка соединения
-        $conn_id = ftp_connect(config('cme_ftp.url'));
+        // $conn_id = ftp_connect(config('cme_ftp.url'));
         // вход с именем пользователя и паролем
-        $login_result = ftp_login($conn_id, config('cme_ftp.user'), '');
+        // $login_result = ftp_login($conn_id, config('cme_ftp.user'), '');
 
-        if ($login_result == true) {
-            ftp_pasv($conn_id, true);
+        // if ($login_result == true) {
+        //    ftp_pasv($conn_id, true);
 
             // получить содержимое текущей директории
-            $contents = ftp_nlist($conn_id, config('cme_ftp.ftp_folder') . '/');
+        //     $contents = ftp_nlist($conn_id, config('cme_ftp.ftp_folder') . '/');
 
-            if (!empty($contents)) {
-                $last_file = array_pop($contents);
-                $last_file = str_replace(config('cme_ftp.ftp_folder') . '/', '', $last_file);
+        //     if (!empty($contents)) {
+        //         $last_file = array_pop($contents);
+        //         $last_file = str_replace(config('cme_ftp.ftp_folder') . '/', '', $last_file);
 
-                $name_arr = explode('-', $last_file);
+        //         $name_arr = explode('-', $last_file);
 
-                $disk->makeDirectory(config('cme_ftp.save_path') . '/' . $name_arr[1] . '/');
+        //         $disk->makeDirectory(config('cme_ftp.save_path') . '/' . $name_arr[1] . '/');
 
                 // попытка скачать и распаковать архив
-                $local_file = config('cme_ftp.save_path') . '/' . $name_arr[1] . '/' . $last_file;
-                $ftp_file = 'ftp://' . config('cme_ftp.url') . '/' . config('cme_ftp.ftp_folder') . '/' . $last_file;
+        //         $local_file = config('cme_ftp.save_path') . '/' . $name_arr[1] . '/' . $last_file;
+        //         $ftp_file = 'ftp://' . config('cme_ftp.url') . '/' . config('cme_ftp.ftp_folder') . '/' . $last_file;
 
-                if (!$disk->has($local_file)) {
-                    if (copy($ftp_file, $path_prefix . $local_file)) {
+        //         if (!$disk->has($local_file)) {
+        //             if (copy($ftp_file, $path_prefix . $local_file)) {
 
-                    } else {
-                        Log::warning('Файл не смогли скопировать.', [ 'file' => $local_file ]);
-                    }
-                } else {
-                    Log::warning('Файл уже скачан.', [ 'file' => $local_file ]);
-                }
+        //             } else {
+        //                 Log::warning('Файл не смогли скопировать.', [ 'file' => $local_file ]);
+        //             }
+        //         } else {
+        //             Log::warning('Файл уже скачан.', [ 'file' => $local_file ]);
+        //         }
 
-                if ($local_file) {
-                    $forward_point = new ForwardPointParser();
+        //         if ($local_file) {
+        //             $forward_point = new ForwardPointParser();
 
-                    $forward_point->parse($local_file);
-                }
-            }
-        }
+        //             $forward_point->parse($local_file);
+        //         }
+        //     }
+        // }
     }
 }
