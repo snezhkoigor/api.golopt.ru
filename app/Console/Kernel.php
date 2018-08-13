e01381826d852f70eb86022fd094288b2bce9552
snezhkoigor <i.s.sergeevich@yandex.ru>
2018-08-13 10:48:49 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 		$schedule->command('getFuturesFromCME')
-			->hourlyAt(17)
+			->hourlyAt(7)
 			->withoutOverlapping();
 
 
