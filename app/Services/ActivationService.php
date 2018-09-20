b1bd03b2ed4030c3065fc41ecc7467aefabddff7
igorsnezko <i.s.sergeevich@yandex.ru>
2018-09-20 17:28:52 +0300

    смена серивиса отправки смс

--- app/Services/ActivationService.php
+++ app/Services/ActivationService.php
@@ -41,7 +41,8 @@ class ActivationService
         }
 
         $code = $this->activationRepo->create($user, true);
-        $send_request = StreamTelecom::PostConnect('http://gateway.api.sc/rest/Send/SendSms/', 'sessionId='.$result.'&sourceAddress=Goloption&destinationAddress='.$user->calling_code.$user->phone.'&data='.view('sms.activation.' . User::getLanguage($user->country), ['code' => $code]));
+        $msg = view('sms.activation.' . User::getLanguage($user->country), ['code' => $code]);
+        $send_request = StreamTelecom::PostConnect('http://gateway.api.sc/rest/Send/SendSms/', 'sessionId='.$result.'&sourceAddress=Goloption&destinationAddress='.$user->calling_code.$user->phone.'&data='.$msg);
 var_dump($send_request);die;
         if (!empty($send_request['Code']))
         {
--- resources/views/sms/activation/en.blade.php
+++ resources/views/sms/activation/en.blade.php
@@ -1,4 +1,1 @@
 For activation enter: {{ $code }}
-
-Best regards,
-{{ config('app.name') }}
