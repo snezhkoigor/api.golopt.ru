e24a375241c49ca8e0e63519467a779fbd556bb1
igorsnezko <i.s.sergeevich@yandex.ru>
2018-09-20 17:35:07 +0300

    смена серивиса отправки смс

--- app/Services/ActivationService.php
+++ app/Services/ActivationService.php
@@ -41,7 +41,7 @@ class ActivationService
         }
 
         $code = $this->activationRepo->create($user, true);
-        $msg = view('sms.activation.' . User::getLanguage($user->country), ['code' => $code]);
+        $msg = 'Code - ' . $code;
         $send_request = StreamTelecom::PostConnect('http://gateway.api.sc/rest/Send/SendSms/', 'sessionId='.$result.'&sourceAddress=Goloption&destinationAddress='.$user->calling_code.$user->phone.'&data='.$msg);
 var_dump($msg);die;
         if (!empty($send_request['Code']))
