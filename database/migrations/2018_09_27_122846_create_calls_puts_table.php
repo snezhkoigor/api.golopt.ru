d00b0a84680d3c538e22e0b80d4b39a9c7da2537
igorsnezko <i.s.sergeevich@yandex.ru>
2018-10-01 11:43:59 +0300

    добавение парсера недельных опционов из файлов

--- database/migrations/2018_09_27_122846_create_calls_puts_table.php
+++ database/migrations/2018_09_27_122846_create_calls_puts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOptionsTable extends Migration
+class CreateCallsPutsTable extends Migration
 {
     /**
      * Run the migrations.
